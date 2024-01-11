64950d5bcbd6b0c3516e3aa7de212a05b700d413
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-11 15:15:04 -0300

    feat: create entity Credit

---
+++ src/main/kotlin/com/tonyycruz/credit/application/system/entity/Credit.kt
@@ -0,0 +1,31 @@
+package com.tonyycruz.credit.application.system.entity
+
+import com.tonyycruz.credit.application.system.enums.Status
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
+
+@Entity
+data class Credit(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false, unique = true)
+    val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false)
+    val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val dayFirstInstallment: LocalDate,
+    @Column(nullable = false)
+    val numberOfInstallments: Int = 0,
+    @Enumerated
+    val status: Status = Status.IN_PROGRESS,
+    @ManyToOne
+    val customer: Customer? = null
+)
