20441ea1c58b3f5f6daa1451580ed58ad903b9c3
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-17 20:01:49 -0300

    feat: create a validation anotate to venidate the first installment

---
+++ src/main/kotlin/com/tonyycruz/credit/application/system/validation/MaxMonthValidator.kt
@@ -0,0 +1,18 @@
+package com.tonyycruz.credit.application.system.validation
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import java.time.LocalDate
+
+class MaxMonthValidator: ConstraintValidator<MaxMonth, LocalDate> {
+    private var maxMonth: Long = 0
+
+    override fun initialize(maxMonth: MaxMonth) {
+        this.maxMonth = maxMonth.max
+    }
+
+    override fun isValid(value: LocalDate, context: ConstraintValidatorContext?): Boolean {
+        val limitDate: LocalDate = LocalDate.now().plusMonths(maxMonth)
+        return value.isBefore(limitDate) || value.isEqual(limitDate)
+    }
+}
