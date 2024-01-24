77ab9f08f5c112eb9f59327a42cb0b67bbee27c8
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-24 17:03:43 -0300

    test: create tests to credit service test cases to find by credit code with invalid data

--- src/test/kotlin/com/tonyycruz/credit/application/system/service/CreditServiceTest.kt
+++ src/test/kotlin/com/tonyycruz/credit/application/system/service/CreditServiceTest.kt
@@ -2,6 +2,8 @@ package com.tonyycruz.credit.application.system.service
 
 import com.tonyycruz.credit.application.system.entity.Credit
 import com.tonyycruz.credit.application.system.entity.Customer
+import com.tonyycruz.credit.application.system.exception.NotFoundException
+import com.tonyycruz.credit.application.system.exception.UnauthorizedException
 import com.tonyycruz.credit.application.system.repository.CreditRepository
 import com.tonyycruz.credit.application.system.service.impl.CreditService
 import com.tonyycruz.credit.application.system.service.impl.CustomerService
@@ -11,14 +13,13 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import org.assertj.core.api.Assertions
-import org.hibernate.validator.constraints.Range
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.ActiveProfiles
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
-import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
@@ -40,10 +41,9 @@ class CreditServiceTest {
         verify(exactly = 1) { creditRepository.save(fakeCredit) }
     }
 
-    @Test fun `should find all credits by customer id`() {
-        val customerId: Long = 1120
-        val fakeCreditList: MutableList<Credit> = mutableListOf()
-        repeat(5) { fakeCreditList.add(buildCredit(customerId = customerId)) }
+    @Test fun `Should find all credits by customer id`() {
+        val customerId: Long = 1120L
+        val fakeCreditList: List<Credit> = buildManyCredits(quantity = 5, customerId = customerId)
         every { creditRepository.findAllByCustomerId(customerId) } returns fakeCreditList
         val current = creditService.findAllByCustomerId(customerId)
 
@@ -51,6 +51,48 @@ class CreditServiceTest {
         Assertions.assertThat(current).size().isEqualTo(5)
         Assertions.assertThat(current).isSameAs(fakeCreditList)
         verify(exactly = 1) { creditRepository.findAllByCustomerId(customerId) }
+    }
+
+    @Test
+    fun `Should find credit by credit code`() {
+        val customerId: Long = 2201L
+        val fakeCredit: Credit = buildCredit(customerId = customerId)
+        every { creditRepository.findByCreditCode(fakeCredit.creditCode) } returns Optional.of(fakeCredit)
+        val current: Credit = creditService.findByCreditCode(id = customerId, creditCode = fakeCredit.creditCode)
+
+        Assertions.assertThat(current).isNotNull
+        Assertions.assertThat(current).isSameAs(fakeCredit)
+        verify(exactly = 1) { creditRepository.findByCreditCode(creditCode = fakeCredit.creditCode) }
+    }
+
+    @Test
+    fun `Should not find a credit with invalid credit code and throws an exception`() {
+        val fakeCreditCode: UUID = UUID.randomUUID()
+        val fakeCustomerId: Long = Random.nextLong()
+        every { creditRepository.findByCreditCode(fakeCreditCode) } returns Optional.empty()
+
+        Assertions.assertThatExceptionOfType(NotFoundException::class.java)
+            .isThrownBy { creditService.findByCreditCode(fakeCustomerId, fakeCreditCode) }
+            .withMessage("Credit code: $fakeCreditCode was not found.")
+        verify(exactly = 1) { creditRepository.findByCreditCode(fakeCreditCode) }
+    }
+
+    @Test
+    fun `Should not access credit if is not the owner`() {
+        val fakeCredit: Credit = buildCredit(customerId = 100L)
+        val invalidCustomerId: Long = 999L
+        every { creditRepository.findByCreditCode(fakeCredit.creditCode) } returns Optional.of(fakeCredit)
+
+        Assertions.assertThatExceptionOfType(UnauthorizedException::class.java)
+            .isThrownBy { creditService.findByCreditCode(id = invalidCustomerId, creditCode = fakeCredit.creditCode) }
+            .withMessage("You do not have permission to access this credit.")
+        verify(exactly = 1) { creditRepository.findByCreditCode(fakeCredit.creditCode) }
+    }
+
+    private fun buildManyCredits(quantity: Int, customerId: Long = 1): List<Credit> {
+        val fakeCreditList: MutableList<Credit> = mutableListOf()
+        repeat(quantity) { fakeCreditList.add(buildCredit(customerId = customerId)) }
+        return fakeCreditList
     }
 
     private fun buildCredit(
