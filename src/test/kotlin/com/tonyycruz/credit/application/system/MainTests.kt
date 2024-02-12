879893cb9a4baeb7de20cbde62352a92c85b08a1
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-02-12 14:00:24 -0300

    refactor: change main class name

--- src/test/kotlin/com/tonyycruz/credit/application/system/MainTests.kt
+++ src/test/kotlin/com/tonyycruz/credit/application/system/MainTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class CreditApplicationSystemApplicationTests {
+class MainTests {
 
 	@Test
 	fun contextLoads() {
