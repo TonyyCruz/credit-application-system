49120c21ab863733f639e46b2fb78aab1fa2e662
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-02-13 13:32:32 -0300

    feat: change the credit view dto to return two decimals on credit value field

--- src/main/kotlin/com/tonyycruz/credit/application/system/dto/response/CreditViewList.kt
+++ src/main/kotlin/com/tonyycruz/credit/application/system/dto/response/CreditViewList.kt
@@ -2,6 +2,7 @@ package com.tonyycruz.credit.application.system.dto.response
 
 import com.tonyycruz.credit.application.system.entity.Credit
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.util.UUID
 
 data class CreditViewList(
@@ -11,7 +12,7 @@ data class CreditViewList(
 ) {
     constructor(credit: Credit) : this(
         creditCode = credit.creditCode,
-        creditValue = credit.creditValue,
+        creditValue = credit.creditValue.setScale(2, RoundingMode.DOWN),
         numberOfInstallments = credit.numberOfInstallments
     )
 }
